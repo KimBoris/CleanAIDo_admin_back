6687099566e32b936c8e8adbdb81c9722a5bdd8f
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-10-18 16:36:15 +0900

    청소아이하니 관리자 백 프로젝트 생성

---
+++ src/main/java/org/zerock/cleanaido_admin_back/CleanAiDoAdminBackApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.cleanaido_admin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanAiDoAdminBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanAiDoAdminBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/cleanaido_admin_back/CleanAiDoAdminBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.cleanaido_admin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanAiDoAdminBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
