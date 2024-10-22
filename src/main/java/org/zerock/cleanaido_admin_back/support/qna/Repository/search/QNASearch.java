6f5ef1a8ae459034502864e71fcfca9a5c86d3f9
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-10-22 16:39:10 +0900

    PHJ_qna read 초기작업, ãlist수정

--- src/main/java/org/zerock/cleanaido_admin_back/support/qna/Repository/search/QNASearch.java
+++ src/main/java/org/zerock/cleanaido_admin_back/support/qna/Repository/search/QNASearch.java
@@ -7,7 +7,12 @@ import org.zerock.cleanaido_admin_back.common.dto.PageResponseDTO;
 import org.zerock.cleanaido_admin_back.support.qna.dto.QuestionListDTO;
 import org.zerock.cleanaido_admin_back.support.qna.entity.Question;
 
+import java.util.Optional;
+
 public interface QNASearch {
     Page<Question> list(Pageable pageable);
+
+    // 새로운 메소드 추가
+    Optional<QuestionListDTO> getAQuestion(Long qno);
 
 }
