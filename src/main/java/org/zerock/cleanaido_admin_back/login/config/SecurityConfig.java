7efebdfe10fa1d8b46789a2c1145b9566a0e7a03
KimBoris <zlas2345@gmail.com>
2024-11-26 15:41:18 +0900

    KBK_ProductController, SecurityConfig

--- src/main/java/org/zerock/cleanaido_admin_back/login/config/SecurityConfig.java
+++ src/main/java/org/zerock/cleanaido_admin_back/login/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) ->
                         auth
                                 .requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/product/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JWTFilter(jwtUtil), org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
