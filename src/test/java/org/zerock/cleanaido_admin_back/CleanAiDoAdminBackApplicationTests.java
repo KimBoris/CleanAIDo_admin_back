5570f164edb5382dc024b2c34e948f3af0a16cc6
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-12-12 13:37:31 +0900

    PHJ_testcode 주석처리

--- src/test/java/org/zerock/cleanaido_admin_back/CleanAiDoAdminBackApplicationTests.java
+++ src/test/java/org/zerock/cleanaido_admin_back/CleanAiDoAdminBackApplicationTests.java
@@ -1,89 +1,89 @@
-package org.zerock.cleanaido_admin_back;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
-import org.zerock.cleanaido_admin_back.product.entity.Product;
-import org.zerock.cleanaido_admin_back.product.repository.ProductRepository;
-import org.zerock.cleanaido_admin_back.support.faq.entity.FAQ;
-import org.zerock.cleanaido_admin_back.support.faq.repository.FAQRepository;
-import org.zerock.cleanaido_admin_back.support.qna.Repository.QuestionRepository;
-import org.zerock.cleanaido_admin_back.support.qna.entity.Question;
-
-import java.time.LocalDateTime;
-
-@SpringBootTest
-class CleanAiDoAdminBackApplicationTests {
-
-
-    @Autowired
-    ProductRepository repository;
-
-    @Autowired
-    FAQRepository faqRepository;
-
-    @Autowired
-    QuestionRepository questionRepository;
-
-    @Test
-    @Transactional
-    @Commit
-    public void testDummies() {
-        LocalDateTime releaseDate = LocalDateTime.of(2024, 11, 26, 11, 0, 58);
-
-        for(int i = 0; i < 10; i++) {
-            Product product = Product.builder()
-                    .pcode("z1kdf" + i)
-                    .pname("product" + i)
-                    .price(10000 + i * 1000)
-                    .quantity(i)
-                    .releasedAt(releaseDate)
-                    .pstatus("판매중")
-                    .ptags("액체세제,인체무해성분")
-//                    .sellerId("s30")
-                    .build();
-
-            repository.save(product);
-        }
-    }
-
-    @Test
-    @Transactional
-    @Commit
-    public void testDummiesFAQ() {
-        for(int i = 0; i < 100; i++)
-        {
-            FAQ faq = FAQ.builder()
-                    .question("TITLE "+i)
-//                    .answer("DES "+"TITLE "+i)
-                    .build();
-
-            faqRepository.save(faq);
-
-
-        }
-    }
-
-    @Test
-    @Transactional
-    @Commit
-    public void testDummiesQuestion() {
-        for(int i = 0; i < 180; i++)
-        {
-            Question qus = Question.builder()
-                    .title("TITLE "+i)
-                    .contents("DES "+"TITLE "+i)
-                    .writer("user"+i)
-                    .build();
-
-            questionRepository.save(qus);
-
-
-        }
-    }
-    
-}
+//package org.zerock.cleanaido_admin_back;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.test.annotation.Commit;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.zerock.cleanaido_admin_back.product.entity.Product;
+//import org.zerock.cleanaido_admin_back.product.repository.ProductRepository;
+//import org.zerock.cleanaido_admin_back.support.faq.entity.FAQ;
+//import org.zerock.cleanaido_admin_back.support.faq.repository.FAQRepository;
+//import org.zerock.cleanaido_admin_back.support.qna.Repository.QuestionRepository;
+//import org.zerock.cleanaido_admin_back.support.qna.entity.Question;
+//
+//import java.time.LocalDateTime;
+//
+//@SpringBootTest
+//class CleanAiDoAdminBackApplicationTests {
+//
+//
+//    @Autowired
+//    ProductRepository repository;
+//
+//    @Autowired
+//    FAQRepository faqRepository;
+//
+//    @Autowired
+//    QuestionRepository questionRepository;
+//
+//    @Test
+//    @Transactional
+//    @Commit
+//    public void testDummies() {
+//        LocalDateTime releaseDate = LocalDateTime.of(2024, 11, 26, 11, 0, 58);
+//
+//        for(int i = 0; i < 10; i++) {
+//            Product product = Product.builder()
+//                    .pcode("z1kdf" + i)
+//                    .pname("product" + i)
+//                    .price(10000 + i * 1000)
+//                    .quantity(i)
+//                    .releasedAt(releaseDate)
+//                    .pstatus("판매중")
+//                    .ptags("액체세제,인체무해성분")
+////                    .sellerId("s30")
+//                    .build();
+//
+//            repository.save(product);
+//        }
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Commit
+//    public void testDummiesFAQ() {
+//        for(int i = 0; i < 100; i++)
+//        {
+//            FAQ faq = FAQ.builder()
+//                    .question("TITLE "+i)
+////                    .answer("DES "+"TITLE "+i)
+//                    .build();
+//
+//            faqRepository.save(faq);
+//
+//
+//        }
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Commit
+//    public void testDummiesQuestion() {
+//        for(int i = 0; i < 180; i++)
+//        {
+//            Question qus = Question.builder()
+//                    .title("TITLE "+i)
+//                    .contents("DES "+"TITLE "+i)
+//                    .writer("user"+i)
+//                    .build();
+//
+//            questionRepository.save(qus);
+//
+//
+//        }
+//    }
+//
+//}
--- src/test/java/org/zerock/cleanaido_admin_back/PasswordEncoderGenerator.java
+++ src/test/java/org/zerock/cleanaido_admin_back/PasswordEncoderGenerator.java
@@ -1,35 +1,35 @@
-package org.zerock.cleanaido_admin_back;
-
-import io.jsonwebtoken.security.Keys;
-import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.security.Key;
-
-public class PasswordEncoderGenerator {
-    @Test
-    public void generateEncodedPassword() {
-        // 비밀번호 평문
-        String rawPassword = "1111";
-
-        // BCryptPasswordEncoder를 이용해 암호화
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(rawPassword);
-
-        // 결과 출력
-        System.out.println("Encoded Password: " + encodedPassword);
-
-        // 암호화된 비밀번호가 유효한지 확인 (테스트 검증)
-        boolean matches = encoder.matches(rawPassword, encodedPassword);
-        System.out.println("Password matches: " + matches);
-    }
-
+//package org.zerock.cleanaido_admin_back;
+//
+//import io.jsonwebtoken.security.Keys;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import java.security.Key;
+//
+//public class PasswordEncoderGenerator {
 //    @Test
-//    public class KeyGenerator {
-//        public static void main(String[] args) {
-//            Key key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
-//            System.out.println("Generated Key: " + java.util.Base64.getEncoder().encodeToString(key.getEncoded()));
-//        }
+//    public void generateEncodedPassword() {
+//        // 비밀번호 평문
+//        String rawPassword = "1111";
+//
+//        // BCryptPasswordEncoder를 이용해 암호화
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String encodedPassword = encoder.encode(rawPassword);
+//
+//        // 결과 출력
+//        System.out.println("Encoded Password: " + encodedPassword);
+//
+//        // 암호화된 비밀번호가 유효한지 확인 (테스트 검증)
+//        boolean matches = encoder.matches(rawPassword, encodedPassword);
+//        System.out.println("Password matches: " + matches);
 //    }
-}
+//
+////    @Test
+////    public class KeyGenerator {
+////        public static void main(String[] args) {
+////            Key key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+////            System.out.println("Generated Key: " + java.util.Base64.getEncoder().encodeToString(key.getEncoded()));
+////        }
+////    }
+//}
